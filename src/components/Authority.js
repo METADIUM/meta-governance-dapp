28a93e7f9f9995de701fb721d29ca1ae9d30eb4b
omnipede <omnipede@naver.com>
2019-02-13 10:21:21 +0900

    Can read meta-authorities/authorities.json

--- src/components/Authority.js
+++ src/components/Authority.js
@@ -3,6 +3,8 @@ import { Button, List, Progress } from 'antd'
 import './style/style.css'
 import { testData } from './test/testData'
 
+import { getGithubContents } from '../util'
+
 class Authority extends React.Component {
     data = {
       authorityItems: []
@@ -24,11 +26,13 @@ class Authority extends React.Component {
       window.open('https://docs.google.com/forms/d/e/1FAIpQLSfpSAevry4nqjljMACD1DhVzP8oU9J0OgvN49bGakofcZa49w/viewform?fbzx=2570300132786392930', '_blank')
     }
 
-    getAuthorityList () {
+    async getAuthorityList () {
       let list = []
+      testData.govTestData = await getGithubContents('METADIUM', 'meta-authorities', 'master', 'authorities.json');
+
       console.log('getAuthorityList length: ', testData.govTestData.length)
 
-      testData.govTestData.map(item => {
+      testData.govTestData.map((item) => {
         list.push(
           <div className='authorityComp'>
             <div style={{ float: 'left', width: '19%', backgroundColor: '#FFEAF6' }}>
@@ -36,12 +40,12 @@ class Authority extends React.Component {
             </div>
             <div style={{ padding: 30, float: 'left', width: '81%' }}>
               <div style={{ height: '70px' }}>
-                <h2 style={{ float: 'left' }}>{item.company}</h2>
-                <h4 style={{ float: 'right' }}>Address: {item.propasalAddr}</h4>
+                <h2 style={{ float: 'left' }}>{item.title}</h2>
+                <h4 style={{ float: 'right' }}>Address: {item.addr}</h4>
               </div>
               <div style={{ height: '80px' }}><p>{item.description}</p></div>
               <div style={{ height: '80px' }}>
-                <div><h3><a href='http://www.metadium.com' target='_blank'>{item.company} Web site</a></h3></div>
+                <div><h3><a href='http://www.metadium.com' target='_blank'>{item.title} Web site</a></h3></div>
                 <div>
                   {this.getSNSList(item.sns)}
                 </div>
