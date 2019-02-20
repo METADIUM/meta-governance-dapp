ff9fc15b4ca54f67f1413bca4b2aed183b2f5b96
omnipede <omnipede@naver.com>
2019-02-20 10:56:19 +0900

    Updated form data.

--- src/components/ProposalForm.js
+++ src/components/ProposalForm.js
@@ -9,28 +9,36 @@ const { TextArea } = Input
 
 class ProposalForm extends React.Component {
 
+    initialFormData = {
+      add: {
+        metaAmountToBeLocked: 0,
+        newAuthorityAddress: 0,
+        newAuthorityNodeDescription: '',
+        description: '',
+      },
+      replace: {
+        metaAmountToBeLockedNew: 0,
+        newAuthorityAddress: 0,
+        newAuthorityNodeDescription: '',
+        metaAmountToBeUnlockedOld: 0,
+        oldAuthorityAddress: 0,
+        oldAuthorityNodeDescription: '',
+        description: '',
+      }
+    }
     data = {
-      selectedVoteTopic: ''
+      selectedVoteTopic: '',
+      formData: null
     }
     state = {
       isBack: false,
       selectedChange: false,
-      metaAmountToBeLocked: null,
-      metaAmountToBeLockedNew: null,
-      newAuthorityAddress: null,
-      newAuthorityNodeDescription: null,
-      metaAmountToBeUnlockedOld: null,
-      oldAuthorityAddress: null,
-      oldAuthorityNodeDescription: null,
-      description: null
-    }
-
-    resetInputStates() {
-      document.getElementById('authorityForm').reset();
+      formData: null
     }
 
     constructor (props) {
       super(props)
+      this.data.formData = JSON.parse(JSON.stringify(this.initialFormData))
     }
 
     componentWillMount () {
@@ -38,34 +46,40 @@ class ProposalForm extends React.Component {
     }
 
     onSelectChange = (value) => {
-      if (this.state.selectedChange){
-        this.resetInputStates();
-      }
       console.log('onSelectChange: ', value)
       this.data.selectedVoteTopic = value
       this.setState({ selectedChange: true })
+
+      /* Reset form field. */
+      this.props.form.resetFields();
+      this.data.formData = JSON.parse(JSON.stringify(this.initialFormData))
     }
 
+    /* Type casting and save form data. */
     handleChange = (e) => {
-      this.setState({[e.target.name]: e.target.value})
+      const {name, value} = e.target;
+      const type = typeof this.data.formData[this.data.selectedVoteTopic][name]
+      this.data.formData[this.data.selectedVoteTopic][name]
+        = (type === 'number') ? Number(value)
+        : value;
     }
 
-    handleSubmit = (e) => {
-      /* Type casting for numerical values. */
-      this.setState({
-        metaAmountToBeLocked: Number(this.state.metaAmountToBeLocked),
-        metaAmountToBeLockedNew: Number(this.state.metaAmountToBeLockedNew),
-        newAuthorityAddress: Number(this.state.newAuthorityAddress),
-        metaAmountToBeUnlockedOld: Number(this.state.metaAmountToBeUnlockedOld),
-        oldAuthoirtyAddress: Number(this.state.oldAuthorityAddress)
-      });
-
-      /* Debugging. */
-      alert(`${this.state.metaAmountToBeLocked}`)
+    /* Submit form data. */
+    handleSubmit = async (e) => {
       e.preventDefault()
+      let submitedFields = Object.keys(this.data.formData[this.data.selectedVoteTopic])
+      this.props.form.validateFields(submitedFields, async (err, values) =>{
+        if (!err){
+          console.log('Received values of form: ', values);
+          await this.setState({
+            formData: this.data.formData
+          })
+          console.log(this.state.formData);
+        }
+      })
     }
 
-    getAuthorityForm () {
+    getProposalForm () {
 
       if (!this.state.selectedChange) return
 
@@ -73,7 +87,11 @@ class ProposalForm extends React.Component {
       let scale = 'META';
       let star = <span style={{color: 'red'}}>*</span>
 
-      function basicRule(msg){
+      const {
+        getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
+      } = this.props.form;
+
+      function defaultConfig(msg){
         return ({
           rules: [{
             required: true,
@@ -81,61 +99,29 @@ class ProposalForm extends React.Component {
           }]
         })
       }
-      function formItemInput(ruleObject, options) {
+
+      function formItemInput(config, options) {
         return (
           <Form.Item>
-            {getFieldDecorator(options.name, ruleObject)(
+            {getFieldDecorator(options.name, config)(
               <Input style= {options.style} name={options.name} onChange={options.onChange} addonAfter={options.addonAfter} />
             )}
           </Form.Item>
         );
       }
 
-      const {
-        getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
-      } = this.props.form;
-
-      return (
+      const contentAdd = (
         <div>
-        <Form onSubmit={this.handleSubmit} id='authorityForm'>
-          {this.data.selectedVoteTopic === 'add'
-            ? <div>
-                <h3> META Amount to be locked {star}</h3>
-                {formItemInput( basicRule('Please input ... '),
-                  {name: 'metaAmountToBeLocked', style: inputMarginStyle, onChange: this.handleChange, addonAfter: scale})}
-              </div>
-            : null
-          }
-          {this.data.selectedVoteTopic === 'replace'
-            ? <div>
-                <h3> META Amount to be locked (New) {star}</h3>
-                {formItemInput( basicRule('Please input ... '),
-                  {name: 'metaAmountToBeLockedNew', style: inputMarginStyle, onChange: this.handleChange, addonAfter: scale})}
-              </div>
-            : null
-          }
-
+        <Form onSubmit={this.handleSubmit}>
+          <h3> META Amount to be locked {star}</h3>
+          {formItemInput( defaultConfig('Please input ... '),
+            {name: 'metaAmountToBeLocked', style: inputMarginStyle, onChange: this.handleChange, addonAfter: scale})}
           <h3> New Authority Address {star} </h3>
-          {formItemInput (basicRule('Please input ...'),
+          {formItemInput (defaultConfig('Please input ...'),
             {name: 'newAuthorityAddress', style: inputMarginStyle, onChange: this.handleChange})}
           <h3> New Authority Node Description {star}</h3>
-          {formItemInput (basicRule('Please input ...'),
+          {formItemInput (defaultConfig('Please input ...'),
             {name: 'newAuthorityNodeDescription', style: inputMarginStyle, onChange: this.handleChange})}
-
-          {this.data.selectedVoteTopic === 'replace'
-            ? <div>
-                <h3> META Amount to be unlocked (Old) {star}</h3>
-                {formItemInput (basicRule('Please input ...'),
-                  {name: 'metaAmountToBeUnlockedOld', style: inputMarginStyle, onChange: this.handleChange})}
-                <h3> Old Authority Address {star}</h3>
-                {formItemInput (basicRule('Please input ...'),
-                  {name: 'oldAuthorityAddress', style: inputMarginStyle, onChange: this.handleChange})}
-                <h3> Old Authority Node Description {star}</h3>
-                {formItemInput (basicRule('Please input ...'),
-                  {name: 'oldAuthorityNodeDescription', style: inputMarginStyle, onChange: this.handleChange})}
-              </div>
-            : null
-          }
 
           <h3> Description </h3>
           <TextArea
@@ -147,21 +133,72 @@ class ProposalForm extends React.Component {
           />
 
           <h4 style={{color: 'red', marginTop: '2%'}}>*Mandatory</h4>
-          <div>
+          <Form.Item>
             <Button className='submit_Btn' shape='round' htmlType='submit'>
               submit
             </Button>
-          </div>
+          </Form.Item>
         </Form>
         </div>
-      )
+      );
+
+      const contentReplace = (
+        <div>
+        <Form onSubmit={this.handleSubmit}>
+          <h3> META Amount to be locked (New) {star}</h3>
+          {formItemInput( defaultConfig('Please input ... '),
+            {name: 'metaAmountToBeLockedNew', style: inputMarginStyle, onChange: this.handleChange, addonAfter: scale})}
+          <h3> New Authority Address {star} </h3>
+          {formItemInput (defaultConfig('Please input ...'),
+            {name: 'newAuthorityAddress', style: inputMarginStyle, onChange: this.handleChange})}
+          <h3> New Authority Node Description {star}</h3>
+          {formItemInput (defaultConfig('Please input ...'),
+            {name: 'newAuthorityNodeDescription', style: inputMarginStyle, onChange: this.handleChange})}
+          <h3> META Amount to be unlocked (Old) {star}</h3>
+          {formItemInput (defaultConfig('Please input ...'),
+            {name: 'metaAmountToBeUnlockedOld', style: inputMarginStyle, onChange: this.handleChange, addonAfter: scale})}
+          <h3> Old Authority Address {star}</h3>
+          {formItemInput (defaultConfig('Please input ...'),
+            {name: 'oldAuthorityAddress', style: inputMarginStyle, onChange: this.handleChange})}
+          <h3> Old Authority Node Description {star}</h3>
+          {formItemInput (defaultConfig('Please input ...'),
+            {name: 'oldAuthorityNodeDescription', style: inputMarginStyle, onChange: this.handleChange})}
+
+          <h3> Description </h3>
+          <TextArea
+            rows={4}
+            placeholder='Max. 256 bytes'
+            autosize={{ minRows: 2, maxRows: 4 }}
+            name = 'description'
+            onChange={this.handleChange}
+          />
+
+          <h4 style={{color: 'red', marginTop: '2%'}}>*Mandatory</h4>
+          <Form.Item>
+              <Button className='submit_Btn' shape='round' htmlType='submit'>
+                submit
+              </Button>
+          </Form.Item>
+        </Form>
+        </div>
+      );
+
+      switch(this.data.selectedVoteTopic) {
+        case 'add':
+          return contentAdd;
+          break;
+        case 'replace':
+          return contentReplace;
+          break;
+        default:
+          return;
+      }
     }
 
     render () {
 
       return (
         <div>
-
         {!this.state.isBack
           ?
           <div>
@@ -183,7 +220,7 @@ class ProposalForm extends React.Component {
               </Select><hr />
             </div><br></br><br></br>
             <div style={{marginTop: '2%'}}>
-              {this.getAuthorityForm()}
+              {this.getProposalForm()}
             </div>
           </div>
           </div>
