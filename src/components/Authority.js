504ee73bdd4a2ba919109c31e04ac3dfd11ccd23
hexoul <crosien@gmail.com>
2019-03-29 16:07:57 +0900

    refactor: improve ProposalForm and getSNSList(). fix wrong keyword and indent

--- src/components/Authority.js
+++ src/components/Authority.js
@@ -7,134 +7,136 @@ import { constants } from '../constants'
 import './style/style.css'
 
 class Authority extends React.Component {
-    data = {
-      authorityItems: [],
-      visibleAuthorityItems: []
+  data = {
+    authorityItems: [],
+    visibleAuthorityItems: []
+  }
+
+  state = {
+    getAuthorityInfo: false
+  }
+
+  constructor (props) {
+    super(props)
+    this.onSearchBtnClick = this.onSearchBtnClick.bind(this)
+    this.onReadMoreClick = this.onReadMoreClick.bind(this)
+    this.getAuthorityList = this.getAuthorityList.bind(this)
+    this.initAuthorityHeight = this.initAuthorityHeight.bind(this)
+
+    this.descriptions = []
+    this.readMoreBtns = []
+
+    window.addEventListener('resize', this.initAuthorityHeight)
+  }
+
+  componentDidMount () {
+    this.getAuthorityList()
+  }
+
+  componentDidUpdate () {
+    this.initAuthorityHeight()
+  }
+
+  onSearchBtnClick (str) {
+    str = str.toLowerCase()
+    let authorityItems = []
+    this.data.authorityItems.forEach((value) => {
+      if ([value.props.item.title, value.props.item.addr].some(elem => elem.toLowerCase().indexOf(str) !== -1)) {
+        authorityItems.push(value)
+      }
+    })
+    this.data.visibleAuthorityItems = authorityItems
+    this.setState({ getAuthorityInfo: true })
+  }
+
+  onApplyBtnClick () {
+    // TODO: load URL from authorities.json
+    window.open('https://docs.google.com/forms/d/e/1FAIpQLSfpSAevry4nqjljMACD1DhVzP8oU9J0OgvN49bGakofcZa49w/viewform?fbzx=2570300132786392930', '_blank')
+  }
+
+  onReadMoreClick (index) {
+    const element = this.descriptions[index]
+    const btn = this.readMoreBtns[index]
+    if (element.offsetHeight === constants.authoritieDescriptionHeight) {
+      element.style.height = 'auto'
+      btn.innerHTML = '- Read Less'
+    } else {
+      element.style.height = constants.authoritieDescriptionHeightToPixel
+      btn.innerHTML = '+ Read More'
     }
+  }
 
-    state = {
-      getAuthorityInfo: false
-    }
+  breakLine (description) {
+    var regex = /(<br>)/g
+    return description.split(regex).map((line, index) => line.match(regex) ? <br key={'key_' + index} /> : line)
+  }
 
-    constructor (props) {
-      super(props)
-      this.onSearchBtnClick = this.onSearchBtnClick.bind(this)
-      this.onReadMoreClick = this.onReadMoreClick.bind(this)
-      this.getAuthorityList = this.getAuthorityList.bind(this)
-      this.initAuthorityHeight = this.initAuthorityHeight.bind(this)
+  getSNSList (snsList) {
+    return Object.keys(snsList)
+      .map(key =>
+        <a key={key} className='snsGroup' href={snsList[key]}>
+          <i className={`fab fa-${key} fa-2x`} />
+        </a>
+      )
+  }
 
-      this.descriptions = []
-      this.readMoreBtns = []
-
-      window.addEventListener('resize', this.initAuthorityHeight)
-    }
-
-    componentDidMount () {
-      this.getAuthorityList()
-    }
-
-    componentDidUpdate () {
-      this.initAuthorityHeight()
-    }
-
-    onSearchBtnClick (str) {
-      str = str.toLowerCase()
-      let authorityItems = []
-      this.data.authorityItems.forEach((value) => {
-        if ([value.props.item.title, value.props.item.addr].some(elem => elem.toLowerCase().indexOf(str) !== -1)) {
-          authorityItems.push(value)
-        }
-      })
-      this.data.visibleAuthorityItems = authorityItems
-      this.setState({ getAuthorityInfo: true })
-    }
-
-    onApplyBtnClick () {
-      // TODO: load URL from authorities.json
-      window.open('https://docs.google.com/forms/d/e/1FAIpQLSfpSAevry4nqjljMACD1DhVzP8oU9J0OgvN49bGakofcZa49w/viewform?fbzx=2570300132786392930', '_blank')
-    }
-
-    onReadMoreClick (index) {
-      const element = this.descriptions[index]
-      const btn = this.readMoreBtns[index]
-      if (element.offsetHeight === constants.authoritieDescriptionHeight) {
-        element.style.height = 'auto'
-        btn.innerHTML = '- Read Less'
-      } else {
-        element.style.height = constants.authoritieDescriptionHeightToPixel
-        btn.innerHTML = '+ Read More'
+  async getAuthorityList () {
+    let list = []
+    for (let i = 0; i < Object.keys(this.props.authorityOriginData).length; i++) {
+      let item = this.props.authorityOriginData[i]
+      let isMember = await this.props.contracts.governance.isMember(item.addr)
+      if (isMember) {
+        list.push(<AuthorityItem
+          key={i}
+          item={item}
+          index={i}
+          descriptions={this.descriptions}
+          readMoreBtns={this.readMoreBtns}
+          breakLine={this.breakLine}
+          onReadMoreClick={this.onReadMoreClick}
+          getSNSList={this.getSNSList}
+        />
+        )
       }
     }
+    this.data.authorityItems = list
+    this.data.visibleAuthorityItems = list
+    this.setState({ getAuthorityInfo: true })
+  }
 
-    breakLine (description) {
-      var regex = /(<br>)/g
-      return description.split(regex).map((line, index) => line.match(regex) ? <br key={'key_' + index} /> : line)
-    }
+  initAuthorityHeight () {
+    let index, description, readMoreBtn
+    for (let i = 0; i < this.data.visibleAuthorityItems.length; i++) {
+      index = this.data.visibleAuthorityItems[i].key
+      description = this.descriptions[index]
+      readMoreBtn = this.readMoreBtns[index]
 
-    getSNSList (snsList) {
-      let sns = []
-      for (var key in snsList) {
-        sns.push(<a key={key} className='snsGroup' href={snsList[key]}><i className={`fab fa-${key} fa-2x`} /></a>)
-      }
-      return sns
-    }
-
-    async getAuthorityList () {
-      let list = []
-      for (let i = 0; i < Object.keys(this.props.authorityOriginData).length; i++) {
-        let item = this.props.authorityOriginData[i]
-        let isMember = await this.props.contracts.governance.isMember(item.addr)
-        if (isMember) {
-          list.push(<AuthorityItem
-            key={i}
-            item={item}
-            index={i}
-            descriptions={this.descriptions}
-            readMoreBtns={this.readMoreBtns}
-            breakLine={this.breakLine}
-            onReadMoreClick={this.onReadMoreClick}
-            getSNSList={this.getSNSList} />
-          )
-        }
-      }
-      this.data.authorityItems = list
-      this.data.visibleAuthorityItems = list
-      await this.setState({ getAuthorityInfo: true })
-    }
-
-    initAuthorityHeight () {
-      let index, description, readMoreBtn
-      for (let i = 0; i < this.data.visibleAuthorityItems.length; i++) {
-        index = this.data.visibleAuthorityItems[i].key
-        description = this.descriptions[index]
-        readMoreBtn = this.readMoreBtns[index]
-
-        if (description.scrollHeight > constants.authoritieDescriptionHeight) {
-          readMoreBtn.style.display = 'block'
-        } else {
-          readMoreBtn.style.display = 'none'
-        }
+      readMoreBtn.style.display = 'none'
+      if (description.scrollHeight > constants.authoritieDescriptionHeight) {
+        readMoreBtn.style.display = 'block'
       }
     }
+  }
 
-    render () {
-      return (
-        <div className='background'>
-          <SubHeader
-            netName={this.props.netName}
-            placeholder='Search by Authority Name, Adress'
-            btnText='Apply for Authority'
-            btnFunction={this.onApplyBtnClick}
-            searchFunction={this.onSearchBtnClick}
-          />
+  render () {
+    return (
+      <div className='background'>
+        <SubHeader
+          netName={this.props.netName}
+          placeholder='Search by Authority Name, Adress'
+          btnText='Apply for Authority'
+          btnFunction={this.onApplyBtnClick}
+          searchFunction={this.onSearchBtnClick}
+        />
 
-          <div className='contentDiv container'>
-            <div className='card_container'>
-              {this.state.getAuthorityInfo ? this.data.visibleAuthorityItems : <MiniLoader />}
-            </div>
+        <div className='contentDiv container'>
+          <div className='card_container'>
+            {this.state.getAuthorityInfo ? this.data.visibleAuthorityItems : <MiniLoader />}
           </div>
         </div>
-      )
-    }
+      </div>
+    )
+  }
 }
+
 export { Authority }
